a3a5e4e26b9e28e00b5ff41cceb21665064e5025
DESKTOP-04U7LKM\EdisonRosales <ediomros@espol.edu.ec>
2020-08-13 00:14:30 -0500

    cambios

--- src/app/pages/map/map.page.ts
+++ src/app/pages/map/map.page.ts
@@ -1,15 +1,73 @@
 import { Component, OnInit } from '@angular/core';
+declare var google;
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.page.html',
   styleUrls: ['./map.page.scss'],
 })
+
+
 export class MapPage implements OnInit {
+  map=null;
+  onOf=true;
 
-  constructor() { }
+  
+  directionsService = new google.maps.DirectionsService();
+  directionsDisplay = new google.maps.DirectionsRenderer();
+  
+  origin = { lat: -2.140495, lng: -79.906420 };
 
-  ngOnInit() {
+  destination = { lat: -2.148250, lng: -79.965180 };
+
+  constructor() {
+  }
+
+  ngOnInit(){
+      this.toggleValue;
+      this.loadMap();
+
+  } 
+
+  loadMap() {
+    // create a new map by passing HTMLElement
+    const mapEle: HTMLElement = document.getElementById('map');
+    const indicatorsEle: HTMLElement = document.getElementById('indicators');
+
+    // create map
+    this.map = new google.maps.Map(mapEle, {
+      center: this.origin,
+      zoom: 12
+    });
+    this.directionsDisplay.setMap(this.map);
+    this.directionsDisplay.setPanel(indicatorsEle);
+
+  
+    google.maps.event.addListenerOnce(this.map, 'idle', () => {
+      mapEle.classList.add('show-map');
+      this.calculateRoute();
+    });
+    
+  }
+
+  private calculateRoute(){
+    this.directionsService.route({
+      origin: this.origin,
+      destination: this.destination,
+      travelMode: google.maps.TravelMode.DRIVING,
+    }, (response, status)  => {
+      if (status === google.maps.DirectionsStatus.OK) {
+        this.directionsDisplay.setDirections(response);
+      } else {
+        alert('No se pudo cargar el mapa ' + status);
+      }
+    });
+  }
+  
+  toggleValue(event):void{
+    const isChecked: HTMLElement = document.getElementById('toggle');
+    const value=isChecked.getAttribute("aria-checked");
+    console.log(value);
   }
 
 }
