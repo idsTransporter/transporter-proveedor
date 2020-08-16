25b77a1a276d269c911fb42b6df42d9dc47abe0e
idsTransporter <ids2020transporter@gmail.com>
2020-08-16 11:40:47 -0500

    Notificaciones sin personalizar

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,17 +1,73 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { AuthService } from "../services/auth.service";
+
+//Para las push notifications
+import {
+  Plugins,
+  PushNotification,
+  PushNotificationToken,
+  PushNotificationActionPerformed 
+} from '@capacitor/core';
+
+const { PushNotifications } = Plugins;
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  
   notificaciones: any []=Array(20); 
   constructor(
     private AFauth: AuthService
   ) {}
+
+  
+  ngOnInit() {
+    console.log('Initializing HomePage');
+
+    /*
+    * Solicitar permiso para usar notificaciones push
+    * iOS solicitará al usuario y regresará si les concedió permiso o no
+    * Android sólo concederá sin preguntar
+    */
+    PushNotifications.requestPermission().then( result => {
+      if (result.granted) {
+        //  Regístrese en Apple / Google para recibir push a través de APNS/FCM
+        PushNotifications.register();
+      } else {
+        // Manejo de errores
+        console.error("ERROR> Linea 42 home.page.ts")
+      }
+    });
+
+    PushNotifications.addListener('registration',
+      (token: PushNotificationToken) => {
+        alert('Push registration success, token: ' + token.value);
+      }
+    );
+
+    PushNotifications.addListener('registrationError',
+      (error: any) => {
+        alert('Error on registration: ' + JSON.stringify(error));
+      }
+    );
+
+    PushNotifications.addListener('pushNotificationReceived',
+      (notification: PushNotification) => {
+        alert('Push received: ' + JSON.stringify(notification));
+      }
+    );
+
+    PushNotifications.addListener('pushNotificationActionPerformed',
+      (notification: PushNotificationActionPerformed) => {
+        alert('Push action performed: ' + JSON.stringify(notification));
+      }
+    );
+  }
+
 
   on_logout(){
     this.AFauth.logout();
