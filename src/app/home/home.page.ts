6671925491cf8dd28fa213b58650a1049e8403fa
idsTransporter <ids2020transporter@gmail.com>
2020-08-16 12:27:21 -0500

    Envio unico

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,15 +2,6 @@ import { Component, OnInit } from '@angular/core';
 
 import { AuthService } from "../services/auth.service";
 
-//Para las push notifications
-import {
-  Plugins,
-  PushNotification,
-  PushNotificationToken,
-  PushNotificationActionPerformed 
-} from '@capacitor/core';
-
-const { PushNotifications } = Plugins;
 
 @Component({
   selector: 'app-home',
@@ -23,51 +14,10 @@ export class HomePage implements OnInit {
   constructor(
     private AFauth: AuthService
   ) {}
-
   
-  ngOnInit() {
-    console.log('Initializing HomePage');
-
-    /*
-    * Solicitar permiso para usar notificaciones push
-    * iOS solicitará al usuario y regresará si les concedió permiso o no
-    * Android sólo concederá sin preguntar
-    */
-    PushNotifications.requestPermission().then( result => {
-      if (result.granted) {
-        //  Regístrese en Apple / Google para recibir push a través de APNS/FCM
-        PushNotifications.register();
-      } else {
-        // Manejo de errores
-        console.error("ERROR> Linea 42 home.page.ts")
-      }
-    });
-
-    PushNotifications.addListener('registration',
-      (token: PushNotificationToken) => {
-        alert('Push registration success, token: ' + token.value);
-      }
-    );
-
-    PushNotifications.addListener('registrationError',
-      (error: any) => {
-        alert('Error on registration: ' + JSON.stringify(error));
-      }
-    );
-
-    PushNotifications.addListener('pushNotificationReceived',
-      (notification: PushNotification) => {
-        alert('Push received: ' + JSON.stringify(notification));
-      }
-    );
-
-    PushNotifications.addListener('pushNotificationActionPerformed',
-      (notification: PushNotificationActionPerformed) => {
-        alert('Push action performed: ' + JSON.stringify(notification));
-      }
-    );
+  ngOnInit(): void {
+    throw new Error("Method not implemented.");
   }
-
 
   on_logout(){
     this.AFauth.logout();
