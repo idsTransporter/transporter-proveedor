7e68c64f3eefaff7418a6a05d7bf5b19dfc11688
idsTransporter <ids2020transporter@gmail.com>
2020-08-22 17:28:31 -0500

    Corregido el login

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -14,7 +14,7 @@ export class AuthService {
     private AFauth: AngularFireAuth,
     private router: Router
   ) { }
-  
+
   /**
    * Login de respuesta asincrona que en caso de ser exitosa 
    * devuelve un token (auth) con la informacion de la sesion.
@@ -45,15 +45,18 @@ export class AuthService {
    * redirecciona a la pantalla de login, sino lanza un error.
    * @returns una promesa 
    */
-  async logout() {
-    try {
-      await this.AFauth.signOut();
-      this.router.navigate(['/login']);
-      console.log('Redirigir');
-    }
-    catch (err) {
-      console.error('ERROR> En la auth. Linea 42 in auth.service.ts' + err);
-    }
+  logout() {
+    return this.AFauth.signOut()
+      .then(() => {
+        this.router.navigate(['/login'])
+        console.log('Redirigir')
+      }
+      ).catch(
+        err => {
+          console.error('ERROR> En la auth. Linea 42 in auth.service.ts' + err)
+
+        }
+      )
   }
 
   /**
@@ -62,20 +65,20 @@ export class AuthService {
    * Pdt: Para probar se recomienda usar un email temporal, debidamente registrado como usuario.
    * @param correo_recuperacion (del proveedor) destino donde se enviara el mensaje
    */
-  reset_password(correo_recuperacion:string){
-    if(isNullOrUndefined(correo_recuperacion) || correo_recuperacion==""){
+  reset_password(correo_recuperacion: string) {
+    if (isNullOrUndefined(correo_recuperacion) || correo_recuperacion == "") {
       alert("Debe ingresar un correo electronico.")
-    }else{
+    } else {
       this.AFauth.sendPasswordResetEmail(correo_recuperacion)
-      .then(
-        (res)=>{
-        console.log("Exito!!! se envio")
-        }
-      ).catch(
-        (err) => {
-          console.error("ERROR> Linea 66 auth.service " + err)
-        }
-      )  
+        .then(
+          (res) => {
+            console.log("Exito!!! se envio")
+          }
+        ).catch(
+          (err) => {
+            console.error("ERROR> Linea 66 auth.service " + err)
+          }
+        )
     }
   }
 }
